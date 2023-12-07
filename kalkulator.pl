18683d9453cb4dbb736350747a8edc2451ac3ba9
Marcin Kubiak <marcin.kubiak@atos.net>
2023-12-07 09:51:10 +0100

    Kalkulator v1.1

--- kalkulator.pl
+++ kalkulator.pl
@@ -2,30 +2,29 @@
 use strict;
 use warnings;
 
-#print "Hello, World!\n";
+my @dozwolone_znaki = qw(+ - * /);
 
-print "Podaj liczbe 1: ";
-my $liczba1 = <STDIN>;
-chomp $liczba1;
-
-print "Podaj liczbe 2: ";
-my $liczba2 = <STDIN>;
-chomp $liczba2;
-
-my $suma = $liczba1 + $liczba2;
-print "Suma: $suma \n";
-
-my $roznica = $liczba1 - $liczba2;
-print "Roznica: $roznica \n";
-
-my $iloczyn = $liczba1 * $liczba2;
-print "Suma: $suma \n";
-
-my $iloraz = 0;
-if ($liczba2 ne 0) {
-    $iloraz = $liczba1 / $liczba2; }
-else {
-    $roznica = "inf";
+if (@ARGV ne 3 && $ARGV[0] =~ /^\d+$/ && $ARGV[2] =~ /^\d+$/) {
+    die "Zle dane wejsciowe";
 }
 
-print "Roznica: $roznica";
+my ($liczba1, $operator, $liczba2) = @ARGV;
+
+
+if (!grep { $_ eq $operator} @dozwolone_znaki ) {
+    print "Podaj operator: ";
+    $operator = <STDIN>;
+    chomp $operator;
+}
+
+if ($operator eq '+') {
+    print "Suma: " . ($liczba1 + $liczba2) . "\n";
+} elsif ($operator eq '-') {
+    print "Roznica: " . ($liczba1 - $liczba2) . "\n";
+} elsif ($operator eq '*') {
+    print "Iloczyn: " . ($liczba1 * $liczba2) . "\n";   
+} elsif ($operator eq '/' && $liczba2 ne 0) { 
+    print "Iloraz: " . ($liczba1 / $liczba2) . "\n"; 
+} else {
+    die "Nieznany operator";
+}
